16dc5e11614a28c8e1411199b34029a2d07f7694
Mark Pothecary <markpothers@hotmail.com>
2019-02-11 13:25:29 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    filter = []
+    new_collection = []
+    x = 0
+    while x < collection.length
+      filter.push(yield collection[x])
+        if filter[x] == "true"
+          new_collection.push(collection[1])
+        end
+      x += 1
+    end
+
+
+    return new_collection
+  else
+    print "This block should not run!"
+  end
 end
